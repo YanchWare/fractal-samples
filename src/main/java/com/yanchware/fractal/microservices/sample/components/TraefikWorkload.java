933f3b654d9e6f59c18b7f1721a08dc262146c35
Angelo Agatino Nicolosi <angelonicolosi@yanchware.com>
2022-08-18 10:11:46 +0200

    Fixing build

--- src/main/java/com/yanchware/fractal/microservices/sample/components/TraefikWorkload.java
+++ src/main/java/com/yanchware/fractal/microservices/sample/components/TraefikWorkload.java
@@ -41,6 +41,7 @@ public class TraefikWorkload {
         "metrics",
         false,
         9100,
+        9100,
         "tcp",
         false);
   }
@@ -50,6 +51,7 @@ public class TraefikWorkload {
         "websecure",
         true,
         8443,
+        443,
         "tcp",
         false);
   }
@@ -59,6 +61,7 @@ public class TraefikWorkload {
         "web",
         false,
         8000,
+        80,
         "tcp",
         true);
   }
