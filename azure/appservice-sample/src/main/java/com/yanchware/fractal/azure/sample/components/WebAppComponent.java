0b73005a40c0afdfcbd2b82062ed098c6fdd5a44
simone <simoneb81@gmail.com>
2023-01-17 13:54:05 +0100

    Web app sample

---
+++ azure/appservice-sample/src/main/java/com/yanchware/fractal/azure/sample/components/WebAppComponent.java
@@ -0,0 +1,26 @@
+package com.yanchware.fractal.azure.sample.components;
+
+import com.yanchware.fractal.sdk.domain.entities.livesystem.paas.providers.azure.AzureResourceGroup;
+import com.yanchware.fractal.sdk.domain.entities.livesystem.paas.providers.azure.appservice.AzureWebApp;
+import com.yanchware.fractal.sdk.domain.entities.livesystem.paas.providers.azure.appservice.AzureWebAppHosting;
+import com.yanchware.fractal.sdk.domain.entities.livesystem.paas.providers.azure.cosmos.*;
+
+public class WebAppComponent {
+
+  public static AzureWebApp getJavaWebAppComponent(String id, AzureResourceGroup azureResourceGroup) {
+    return AzureWebApp.builder()
+        .withId(id)
+        .withPrivateSSHKeyPassphraseSecretId("fractal-deployer-secret-id")
+        .withPrivateSSHKeySecretId("fractal-deployer-passphrase-secret-id")
+        .withSSHRepositoryURI("git@github.com:YanchWare/fractal-samples.git")
+        .withRepoId("YanchWare/fractal-samples")
+        .withBranchName("env/prod")
+        .withHosting(
+            AzureWebAppHosting.builder()
+                .withJavaVersion("JAVA:18-java18")
+                .build()
+        )
+    .build();
+  }
+
+}
