73b9fab7fbb9986cd46dcaf3a5f4f0206636c225
simone <simoneb81@gmail.com>
2023-01-25 14:49:56 +0100

    Json string

--- azure/storage-account-sample/src/main/java/com/yanchware/fractal/azure/sample/components/StorageAccountComponent.java
+++ azure/storage-account-sample/src/main/java/com/yanchware/fractal/azure/sample/components/StorageAccountComponent.java
@@ -10,7 +10,6 @@ public class StorageAccountComponent {
   public static AzureStorageAccount getStorageAccountComponent(String id) {
 
     // Backup policy definition:
-    final String backupPolicy = "{\"workLoadType\":\"AzureFileShare\",\"backupManagementType\":\"AzureStorage\",\"timeZone\":\"UTC\",\"schedulePolicy\":{\"schedulePolicyType\":\"SimpleSchedulePolicy\",\"scheduleRunFrequency\":\"Daily\",\"scheduleRunTimes\":[\"2018-01-24T01:00:00.000000000Z\"]},\"retentionPolicy\":{\"retentionPolicyType\":\"LongTermRetentionPolicy\",\"dailySchedule\":{\"retentionDuration\":{\"count\":30,\"durationType\":\"Days\"},\"retentionTimes\":[\"2018-01-24T01:00:00.000000000Z\"]},\"weeklySchedule\":null,\"monthlySchedule\":{\"retentionScheduleFormatType\":\"Daily\",\"retentionScheduleDaily\":{\"daysOfTheMonth\":[{\"date\":1,\"isLast\":false}]},\"retentionDuration\":{\"count\":12,\"durationType\":\"Months\"},\"retentionScheduleWeekly\":null,\"retentionTimes\":[\"2018-01-24T01:00:00.000000000Z\"]},\"yearlySchedule\":null}}";
 
     return AzureStorageAccount.builder()
         .withId(id)
@@ -20,10 +19,46 @@ public class StorageAccountComponent {
                 .build()
         )
         .withBackup(AzureStorageAccountBackup.builder()
-            .withPolicy(backupPolicy)
+            .withPolicy(getBackupPolicy())
             .build()
         )
         .build();
+  }
+  
+  private static String getBackupPolicy() {
+    return """
+            {
+              "workLoadType": "AzureFileShare",
+              "backupManagementType": "AzureStorage",
+              "timeZone": "UTC",
+              "schedulePolicy":
+                {
+                  "schedulePolicyType": "SimpleSchedulePolicy",
+                  "scheduleRunFrequency": "Daily",
+                  "scheduleRunTimes": ["2018-01-24T01:00:00.000000000Z"]
+                },
+              "retentionPolicy":
+                {
+                  "retentionPolicyType": "LongTermRetentionPolicy",
+                  "dailySchedule":
+                    {
+                      "retentionDuration": { "count": 30, "durationType": "Days" },
+                      "retentionTimes": ["2018-01-24T01:00:00.000000000Z"]
+                    },
+                  "weeklySchedule": null,
+                  "monthlySchedule":
+                    {
+                      "retentionScheduleFormatType": "Daily",
+                      "retentionScheduleDaily":
+                        { "daysOfTheMonth": [{ "date": 1, "isLast": false }] },
+                      "retentionDuration": { "count": 12, "durationType": "Months" },
+                      "retentionScheduleWeekly": null,
+                      "retentionTimes": ["2018-01-24T01:00:00.000000000Z"]
+                    },
+                  "yearlySchedule": null
+                }
+            }
+        """;
   }
 
 }
