11bd15341ba31ba6f964b02cb38c041cc9530d01
Flavian Popa <fp@yanchware.com>
2023-02-17 08:22:24 +0200

    Service Bus Example

---
+++ azure/service-bus-full-sample/src/main/java/com/yanchware/fractal/ServiceBusFullSample.java
@@ -0,0 +1,37 @@
+package com.yanchware.fractal;
+
+import com.yanchware.fractal.azure.sample.configuration.EnvVarConfiguration;
+import com.yanchware.fractal.sdk.Automaton;
+import com.yanchware.fractal.sdk.aggregates.Environment;
+import com.yanchware.fractal.sdk.aggregates.LiveSystem;
+import com.yanchware.fractal.sdk.domain.exceptions.InstantiatorException;
+
+import java.util.List;
+
+import static com.yanchware.fractal.azure.sample.components.AzServiceBus.getServiceBus;
+
+public class ServiceBusFullSample {
+  public static void main(String[] args) throws InstantiatorException {
+    // CONFIGURATION:
+    var configuration = EnvVarConfiguration.getInstance();
+
+    var env = Environment.builder()
+        .withId(configuration.getSubscriptionId())
+        .withDisplayName(configuration.getEnvironmentDisplayName())
+        .withParentId(configuration.getTenantId())
+        .withParentType("tenant")
+        .build();
+
+
+    // INSTANTIATION:
+    LiveSystem liveSystem = LiveSystem.builder()
+        .withName(configuration.getLiveSystemName())
+        .withDescription("Service Bus with full options sample")
+        .withResourceGroupId(configuration.getResourceGroupId())
+        .withComponent(getServiceBus())
+        .withEnvironment(env)
+        .build();
+
+    Automaton.instantiate(List.of(liveSystem));
+  }
+}
