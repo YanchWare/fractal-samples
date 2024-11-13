18f78e8896b8345f2361e145b081f5a368fe5bea
Angelo Agatino Nicolosi <aan@yanchware.com>
2024-11-13 22:25:54 +0100

    Add auth middleware

--- environment-initialization/src/main/java/com/yanchware/fractal/samples/environment/initialization/configuration/Constants.java
+++ environment-initialization/src/main/java/com/yanchware/fractal/samples/environment/initialization/configuration/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
   public static final String FRACTAL_RESOURCE_GROUP_ID_ENV_VAR_KEY = "FRACTAL_RESOURCE_GROUP_ID";
   public static final String AZURE_TENANT_ID_ENV_VAR_KEY = "AZURE_TENANT_ID";
   public static final String AZURE_SUBSCRIPTION_ID_ENV_VAR_KEY = "AZURE_SUBSCRIPTION_ID";
-  public static final String AWS_ORGANIZATION_ID_ENV_VAR_KEY = "AWS_ORGANIZATION_ID_ENV";
+  public static final String AWS_ORGANIZATION_ID_ENV_VAR_KEY = "AWS_ORGANIZATION_ID";
   public static final String AWS_ACCOUNT_ID_ENV_VAR_KEY = "AWS_ACCOUNT_ID";
 }
