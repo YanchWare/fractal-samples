f9b0268a574c286206aaab9956529774ec0f435d
Angelo Agatino Nicolosi <aan@yanchware.com>
2024-11-14 09:43:45 +0100

    Move jar details into specific project build file

--- buildSrc/src/main/kotlin/samples.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/samples.java-conventions.gradle.kts
@@ -67,21 +67,3 @@ tasks.named<Test>("test") {
         showFullStackTraces = true
     }
 }
-
-tasks.jar {
-    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA", "META-INF/*.MF")
-
-    manifest {
-        attributes(
-            "Implementation-Version" to archiveVersion,
-            "Main-Class" to "com.yanchware.fractal.samples.environment.initialization.EnvironmentInitializationSample"
-        )
-    }
-
-    val dependencies = configurations
-        .runtimeClasspath
-        .get()
-        .map(::zipTree)
-    from(dependencies)
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
